d4a89aaeddf72009d9cfb749ead4da3fd39b8798
康建 <kangjian@kangjiandeMacBook-Pro.local>
2023-12-28 12:16:43 +0800

    多个回复恢复流式

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from db_process import *
 from RAG_with_langchain import get_cache, get_cache_serial, response_from_rag_chain, response_from_retriver
 from templates import *
 from utils import *
-from geminiai import gemini_response
+from geminiai import gemini_response, gemini_response_key_words
 #from werkzeug.utils import secure_filename
 #from pypinyin import lazy_pinyin
 
@@ -20,56 +20,61 @@ param_temperature = 0.5
 param_n = 3
 #max_k = 10
 
-def Chat_Completion(model, question, tem, messages, stream, n=1):
+def Chat_Completion(model, question, tem, messages, stream, n=param_n):
     try:
         messages.append({"role": "user", "content": question})
-        print("对话长度：", len(messages), "generate_text:", messages)
+        print("generate_text:", messages)
         response = client.chat.completions.create(
         model= model,
         messages= messages,
         temperature=tem,
         stream=stream,
         top_p=1.0,
+        n=n,
         frequency_penalty=0,
-        presence_penalty=0,
-        n=n
+        presence_penalty=0
         )
         if not stream:
             print(f"{response.usage}\n")
             session['tokens'] = response.usage.total_tokens
-            combined_content = '\n'.join([f"******\n回复【{n+1}】:\n{choice.message.content}" for n, choice in enumerate(response.choices)])
-            return combined_content
+            return response.choices[0].message.content
+        else:
+            partial_words = ""
+            text=[''] * n
+            chunk_count = 0
+
+            for chunk in response:
+                if not chunk or not chunk.choices[0].delta:
+                    continue
+
+                choice = chunk.choices[0]
+                # print("Decoded chunk:", choice)  # 添加这一行以打印解码的块
+                content = choice.delta.content if choice.delta.content else ""
+                
+                if choice.index == 0:
+                    # 对于第一个choice，立即输出并重置chunk_count
+                    partial_words += content
+                    chunk_count = 0
+                    yield {'content': partial_words}
+                else:
+                    # 对于其他choices，暂存内容
+                    text[choice.index] += content
+                    # print(choice.index, text[choice.index])
+                    chunk_count += 1
+                    if chunk_count > 4:
+                        yield {'content': partial_words + "\n***请等待全部输出完毕***"}
+
+                if choice.finish_reason == "stop":
+                    break
+        # 在所有响应处理完毕后，输出暂存变量中的内容
+        if text[1]:
+            combined_content = '\n'.join([f"{'-'*10}\n回复 {n+2}：\n{choice}" for n, choice in enumerate(text[1:])])
+            yield {'content': partial_words+'\n'+combined_content}
         return response
         
     except Exception as e:
         print(e)
         return "Connection Error! Please try again."
-
-def send_gpt(model, prompt, tem, messages, user_id):
-    partial_words = ""
-    response = Chat_Completion(model, prompt, tem, messages, True)
-
-    for chunk in response:
-        if chunk:
-            # print("Decoded chunk:", chunk)  # 添加这一行以打印解码的块
-            try:
-                if chunk.choices[0].delta:
-                    finish_reason = chunk.choices[0].finish_reason
-                    if finish_reason == "stop":
-                        break
-                    if chunk.choices[0].delta.content:
-                        partial_words += chunk.choices[0].delta.content
-                        # print("Content found:", partial_words)  # 添加这一行以打印找到的内容
-                        yield {'content': partial_words}
-                    else:
-                        print("No content found in delta:", chunk.choices[0].delta)  # 添加这一行以打印没有内容的 delta
-                else:
-                    pass
-            except json.JSONDecodeError:
-                pass
-
-#        print(f"{response['usage']}\n")
-#        session['tokens'] = response['usage']['total_tokens']
         
 def count_chars(text, user_id, messages):
     cn_pattern = re.compile(r'[\u4e00-\u9fa5\u3000-\u303f\uff00-\uffef]') #匹配中文字符及标点符号
@@ -178,19 +183,19 @@ def handle_message():
             clear_files_with_prefix(user_id)
             session['uploaded_filename'] = ''
             #session['key_words'] = {}
-            return jsonify('Cleared.')
+            return Response('data: {"data": "Cleared."}\n\n', mimetype='text/event-stream')
         elif user_input.startswith('#file'):
             filelist = get_files_with_prefix(user_id) or "没有文件上传。"
-            return jsonify(f'{filelist}') 
+            return Response(f'data: {json.dumps({"data": filelist})}\n\n', mimetype='text/event-stream')
 
         elif uploaded_filename == '' or is_file_in_directory(user_id + '_' + uploaded_filename) == False:
-            return jsonify('请先上传文档')
+            return Response('data: {"data": "请先上传文档。"}\n\n', mimetype='text/event-stream')
         else:
             if user_input.startswith(('总结', '写作')) and num_tokens(user_input) < 20:
                 # 处理提取关键词逻辑
                 try:
                     response = response_from_retriver(user_id+'_'+uploaded_filename, uploaded_filename, 2)
-                    key_words = gemini_response(f"提取以下信息的关键词，以/分隔显示，不超过5个：\n{response}")
+                    key_words = gemini_response_key_words(f"提取以下信息的关键词，以/分隔显示，不超过5个：\n{response}")
                      #session['uploading'] = False
                     #print("\n#Session after key words extracted:", session)        
                 except Exception as e:
@@ -210,25 +215,33 @@ def handle_message():
             prompt = f"{docchat_template.format(question=user_input, context=response)!s}"
             response = interact_with_openai(user_id, prompt, prompt_template)
 
-    return jsonify(response) # 非流式
+    return Response(response, mimetype='text/event-stream') #流式必须要用Response
 
 def interact_with_openai(user_id, prompt, prompt_template, messages=None):
     messages = [] if messages is None else messages
-    res = Chat_Completion(model, prompt, param_temperature, messages, False, param_n) # 非流式调用
-    
-    messages.append({"role": "assistant", "content": res})
-    join_message = "".join([msg["content"] for msg in messages])
-    print("精简前messages:", messages)
-    rows = history_messages(user_id, prompt_template[0]) # 历史记录条数
-    if len(messages) > rows:
-        messages = messages[-rows:] #对话仅保留最新rows条
-    if rows == 0:
-        save_user_messages(user_id, []) # 清空历史记录
-    else:
-        save_user_messages(user_id, messages)
-    # session['messages'] = messages
-    count_chars(join_message, user_id, messages)
-    return res # 非流式
+    res = None
+
+    try:
+        for res in Chat_Completion(model, prompt, param_temperature, messages, True):
+            if 'content' in res:
+                markdown_message = res['content']  # generate_markdown_message(res['content'])
+                # print(f"Yielding markdown_message: {markdown_message}")  # 添加这一行
+                # token_counter += 1
+                yield f"data: {json.dumps({'data': markdown_message})}\n\n" # 将数据序列化为JSON字符串
+    finally:
+        messages.append({"role": "assistant", "content": res})
+        print(messages)
+        join_message = "".join([str(msg["content"]) for msg in messages])
+        print("精简前messages:", messages)
+        rows = history_messages(user_id, prompt_template[0]) # 历史记录条数
+        if len(messages) > rows:
+            messages = messages[-rows:] #对话仅保留最新rows条
+        if rows == 0:
+            save_user_messages(user_id, []) # 清空历史记录
+        else:
+            save_user_messages(user_id, messages)
+        # session['messages'] = messages
+        count_chars(join_message, user_id, messages)
 
 @app.route('/')
 def index():
--- geminiai.py
+++ geminiai.py
@@ -8,5 +8,15 @@ model = genai.GenerativeModel('gemini-pro')
 #odel_v = genai.GenerativeModel('gemini-pro-vision')
 
 def gemini_response(query):
+    response = model.generate_content(query, stream=True)
+
+    partial_text = ''
+    for chunk in response:
+        print(chunk.text)
+        print("_"*80)
+        partial_text += chunk.text
+        yield(f"data: {json.dumps({'data': partial_text})}\n\n")
+
+def gemini_response_key_words(query):
     response = model.generate_content(query, stream=False)
     return response.text
