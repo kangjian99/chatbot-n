c452f691ecfa7c040b58f8cc008b62c5d66cddeb
康建 <kangjian@kangjiandeMacBook-Pro.local>
2024-01-03 11:44:51 +0800

    增加cors

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, Response, session, render_template
-#from flask_cors import CORS
+from flask_cors import CORS
 from openai import OpenAI
 import json, threading
 from datetime import timedelta
@@ -12,7 +12,7 @@ from geminiai import gemini_response, gemini_response_key_words
 #from pypinyin import lazy_pinyin
 
 app = Flask(__name__)
-#CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True)
 app.config['SECRET_KEY'] = SESSION_SECRET_KEY
 app.config['SESSION_PERMANENT'] = True
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=12)  # 无交互session过期（重登录）时间
