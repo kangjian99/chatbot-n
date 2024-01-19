fb84f4f32b7c14b34d39c8e31753320beaf2730c
康建 <kangjian@kangjiandeMacBook-Pro.local>
2024-01-19 13:02:55 +0800

    prompt templates update

--- templates.py
+++ templates.py
@@ -20,7 +20,7 @@ Copy whole paragraphs from context.
 ## Context:
 {context}
 ## Assistant starts writing:
-文章结构清晰，包括attractive headline和子标题，符合用户的字数要求。
+文章结构清晰，包括one redesigned attractive headline and some sub-headlines, 符合用户的字数要求。
 """ # 
 
 template_mimic = """你是一位专业的写作专家，Use the following pieces of context to rewrite an detailed article in Chinese.
