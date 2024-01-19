b938cc4d4f5c491bb5edf266332aebee72f1174f
康建 <kangjian@kangjiandeMacBook-Pro.local>
2024-01-19 11:01:12 +0800

    prompts update

--- templates.py
+++ templates.py
@@ -1,20 +1,26 @@
 template = """You are an assistant for question-answering tasks.
 Use the following pieces of retrieved context to answer the question only in Chinese. Don't try to make up an answer. 
+###
 Question: {question} 
+###
 Context: {context} 
+###
 Assistant answer based on most relevant sentences in the context:
 """ # 最后一句非常关键
 
-template_writer = """你是一位语言优美的写作专家，Use the following pieces of context to rewrite an detailed and structual article in Chinese based on the user question.
-POSITIVE:先思考大纲，用重新组织的文字输出，文章结构包括主标题和子标题，符合用户的字数要求。
-NEGATIVE:Copy whole paragraphs from context.
-Think before response. If you don't know the answer, just say "Sorry, I don't know", don't try to make up an answer. 
-###
-Question: {question}
-###
-Context: {context}
-###
-Assistant writes article:
+template_writer = """你是一位富有经验的写作专家，Use the following pieces of context to rewrite an detailed and structual article in Chinese following the directions of user question.
+Think before response. Don't try to make up an answer. 
+## POSITIVE:
+语言风格生动、用词灵活；
+Reorganize text from the context;
+文章结构清晰，包括主标题和子标题，符合用户的字数要求。
+## NEGATIVE:
+Copy whole paragraphs from context.
+## User Question:
+{question}
+## Context:
+{context}
+## Assistant starts writing:
 """ # 
 
 template_mimic = """你是一位专业的写作专家，Use the following pieces of context to rewrite an detailed article in Chinese.
