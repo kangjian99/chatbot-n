8b82f4e3dfe64c6b2a4eeff0e8f05dc7e7a4c1d2
kangjian99 <radiokj@163.com>
2025-02-11 11:36:33 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,8 +5,7 @@ from openai import OpenAI
 
 API_KEY = os.environ.get('OPENAI_API_KEY')
 
-MODEL_base=os.getenv('GPT_MODEL') or "gpt-4o-2024-08-06" # "gpt-3.5-turbo-0125"
-MODEL=os.getenv('GPT4_MODEL') or MODEL_base
+MODEL=os.getenv('GPT_MODEL') or "gpt-4o-2024-08-06" # "gpt-3.5-turbo-0125"
 #MODEL="gpt-4-turbo-2024-04-09"
 #model_8k = "moonshot-v1-8k"
 #model_32k = "moonshot-v1-32k"
@@ -31,14 +30,18 @@ elif hub == "nv":
     API_KEY_HUB = os.environ.get('NV_API_KEY') 
     BASE_URL = "https://integrate.api.nvidia.com/v1"
     MODEL_base = "deepseek-ai/deepseek-r1"
-elif hub == "moonshot":
-    API_KEY_HUB = os.environ.get('MOONSHOT_API_KEY') 
-    BASE_URL = "https://api.moonshot.cn/v1"
-    MODEL_base = "moonshot-v1-8k"
+#elif hub == "moonshot":
+#    API_KEY_HUB = os.environ.get('MOONSHOT_API_KEY') 
+#    BASE_URL = "https://api.moonshot.cn/v1"
+#    MODEL_base = "moonshot-v1-8k"
 elif hub == "tg":
     API_KEY_HUB = os.environ.get('TOGETHER_API_KEY') 
     BASE_URL = "https://api.together.xyz/v1"
     MODEL_base = "deepseek-ai/DeepSeek-R1"
+elif hub == "fw":
+    API_KEY_HUB = os.environ.get('FIREWORKS_API_KEY') 
+    BASE_URL = "https://api.fireworks.ai/inference/v1"
+    MODEL = "accounts/fireworks/models/deepseek-r1"
 elif hub:
     API_KEY_HUB = os.environ.get('BASE_API_KEY')
     BASE_URL = os.environ.get('BASE_URL') or "https://llm.indrin.cn/v1"
@@ -48,8 +51,17 @@ else:
     BASE_URL = None
 
 client = OpenAI(api_key = API_KEY_HUB, base_url = BASE_URL) if hub else OpenAI(api_key = API_KEY)
-#model_alt = "deepseek-chat"
-#client_alt = OpenAI(api_key = os.environ.get('DEEPSEEK_API_KEY'), base_url = "https://api.deepseek.com/v1")
+#MODEL_alt = ""
+#CLIENT_alt = OpenAI(api_key = os.environ.get('GROQ_API_KEY'), base_url = "https://api.groq.com/openai/v1")
+MODEL_alt = "gemini-2.0-flash"
+CLIENT_alt = OpenAI(api_key = os.environ.get('GOOGLE_API_KEY'), base_url = "https://generativelanguage.googleapis.com/v1beta/openai/")
+
+model_alt_map = {
+    "sf": "deepseek-ai/DeepSeek-V3",
+    "nv": "mistralai/mistral-small-24b-instruct",
+    "tg": "mistralai/Mistral-Small-24B-Instruct-2501",
+    "fw": "accounts/fireworks/models/deepseek-v3",
+}
 
 DB_URL: str = os.environ.get("SUPABASE_URL")
 DB_KEY: str = os.environ.get("SUPABASE_KEY")
