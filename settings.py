e06753000cd6d31853aa7aece3cd7dd43eb5b091
kangjian99 <radiokj@163.com>
2025-03-23 19:57:50 +0800

    更新fw model和随机模型，为novita优化max_tokens

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ elif HUB == "tg":
 elif HUB == "fw":
     API_KEY_HUB = os.environ.get('FIREWORKS_API_KEY') 
     BASE_URL = "https://api.fireworks.ai/inference/v1"
-    MODEL = "accounts/fireworks/models/deepseek-r1"
+    MODEL = "accounts/fireworks/models/deepseek-r1-basic"
 elif HUB == "nb":
     API_KEY_HUB = os.environ.get('NEBIUS_API_KEY')
     BASE_URL = "https://api.studio.nebius.ai/v1"
@@ -73,7 +73,7 @@ model_alt_map = {
     "sf": "deepseek-ai/DeepSeek-R1",
     "nv": "deepseek-ai/deepseek-r1", # "mistralai/mistral-small-24b-instruct",
     "tg": "deepseek-ai/DeepSeek-R1", # "mistralai/Mistral-Small-24B-Instruct-2501",
-    "fw": "accounts/fireworks/models/deepseek-r1", # "accounts/fireworks/models/deepseek-v3",
+    "fw": "accounts/fireworks/models/deepseek-r1-basic", # "accounts/fireworks/models/deepseek-v3",
     "nb": "deepseek-ai/DeepSeek-R1-fast", # "deepseek-ai/DeepSeek-V3",
     "ark": "ep-20250218083204-w9cc9", # "ep-20250219170102-9tb5f",
     "nov": "deepseek/deepseek-r1-turbo",
